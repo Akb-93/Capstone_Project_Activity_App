c3e21a0c9f2ec220efd5f66de9c3215fe59bb68c
Andreas Buchberger <Buchberger.Andreas@icloud.com>
2025-05-07 13:48:51 +0200

    implemented burger menu with with react Link tags

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from "react";
+import Link from "next/link";
 
-//Navbar Component 
 const HeaderComponent = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    
+    useEffect(() => {
+        const handleClickOutside = (event) => { 
+            if (menuRef.current && !menuRef.current.contains(event.target)) { 
+                setIsMenuOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const handleActivitiesClick = () => {
+        setIsMenuOpen(false);
+        console.log("Navigating to Activities");
+    };
+
+    const handleFavoritesClick = () => {
+        setIsMenuOpen(false);
+        console.log("Navigating to Favorites");
+    };
+
     return (
         <header>
-            <section>
+            <div>
                 <div>
                     Logo
                 </div>
-                <div>
-                    Menu
-                </div>
-            </section>
+                <nav ref={menuRef}>
+                    <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                        {isMenuOpen ? (
+                            <img
+                                src="/img/x.svg" 
+                                alt="Close Menu"
+                                size="24px"
+                            />
+                        ) : (
+                            <img
+                                src="/img/menu.svg" 
+                                alt="Open Menu"
+                                size="24px"
+                            />
+                        )}
+                    </button>
+                    {isMenuOpen && (
+                        <ul>
+                            <li>
+                                <Link href="/activities">
+                                    <button onClick={handleActivitiesClick}>
+                                        Activities
+                                    </button>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/favorites">
+                                    <button onClick={handleFavoritesClick}>
+                                        Favorites
+                                    </button>
+                                </Link>
+                            </li>
+                        </ul>
+                    )}
+                </nav>
+            </div>
         </header>
     );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import GlobalStyle from "../styles";
-
+import HeaderComponent from"@/components/Header";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <HeaderComponent/>
       <GlobalStyle />
       <Component {...pageProps} />
     </>

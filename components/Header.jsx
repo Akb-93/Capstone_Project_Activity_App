e7cbf9b01b5856e6c13ebf15b2633a61261f6d74
Andreas Buchberger <Buchberger.Andreas@icloud.com>
2025-05-07 12:01:35 +0200

    basic setup of the Header Component

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//Navbar Component 
+const HeaderComponent = () => {
+    return (
+        <header>
+            <section>
+                <div>
+                    Logo
+                </div>
+                <div>
+                    Menu
+                </div>
+            </section>
+        </header>
+    );
+};
+
+export default HeaderComponent;

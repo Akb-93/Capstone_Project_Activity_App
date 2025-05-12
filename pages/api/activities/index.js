c1999ecbb7a39c35d553ec8c4c147917ae1cdaee
Alissa Svertokina <svertokina92@gmail.com>
2025-05-12 16:38:20 +0200

    Fix merge

--- pages/api/activities/index.js
+++ pages/api/activities/index.js
@@ -1,10 +1,10 @@
 //create a GET API route that fetches activities from your Activity model and populates the categories field with the actual category names
-import dbConnect from '@/db/connect';
-import Activity from '@/db/models/Activities';
+import dbConnect from "@/db/connect";
+import Activity from "@/db/models/Activities";
 
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    res.setHeader('Allow', ['GET']);
+  if (req.method !== "GET") {
+    res.setHeader("Allow", ["GET"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
   try {
@@ -12,12 +12,12 @@ export default async function handler(req, res) {
     await dbConnect();
 
     //fetch activities(all) with categories
-    const activities = await Activity.find().populate('categories');
+    const activities = await Activity.find().populate("categories");
 
     //return activities as JSON
     res.status(200).json(activities);
   } catch (error) {
-    console.error('Error fetching activities:', error);
-    res.status(500).json({ error: 'Failed to fetch activities' });
+    console.error("Error fetching activities:", error);
+    res.status(500).json({ error: "Failed to fetch activities" });
   }
 }

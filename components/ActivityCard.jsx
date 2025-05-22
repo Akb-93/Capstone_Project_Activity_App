c8feadc492d462460c21e53117093d5d0252a770
Angela Capillo <hello@angelcapillo.com>
2025-05-22 15:54:11 +0200

    Fix redundant comma

--- components/ActivityCard.jsx
+++ components/ActivityCard.jsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 export default function ActivityCard({
   activity,
   onFavoriteToggle,
-  isFavorite,
+  isFavorite
 }) {
   const router = useRouter();
   const { pathname: from } = router;

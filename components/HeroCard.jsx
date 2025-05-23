7d5dfc1a8dfd0a957269ccc6ff1ab7699305206e
jessica <jessy_marian91@hotmail.com>
2025-05-23 11:18:00 +0200

    image fixed

--- components/HeroCard.jsx
+++ components/HeroCard.jsx
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
+import Image from "next/image";
+
 export default function HeroCard({ title, subtitle, backgroundImageUrl }) {
   return (
-    <Wrapper backgroundImageUrl={backgroundImageUrl}>
+    <Wrapper>
+      <StyledImage
+        src={backgroundImageUrl}
+        alt="Hero background"
+        fill
+        objectFit="cover"
+        priority
+        quality={80}
+      />
+
       <Overlay />
       <Content>
         <Title>{title}</Title>
@@ -21,12 +32,35 @@ HeroCard.defaultProps = {
 const Wrapper = styled.header`
   position: relative;
   background-image: url(${(props) => props.backgroundImageUrl});
-  background-size: cover;
-  background-position: center;
-  padding: var(--space-5) var(--space-3);
-  border-radius: var(--radius-md);
+  width: 100%; /* Full viewport width */
+  height: 200px; /* Fixed height */
   overflow: hidden;
   color: var(--c-neutral-000);
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%; /* or try 60% to limit the overlay height */
+    background: linear-gradient(to top, rgba(0, 0, 0, 0.8), transparent);
+    z-index: 1;
+    pointer-events: none;
+  }
+`;
+const StyledImage = styled(Image)`
+  position: absolute !important;
+  top: 0;
+  left: 0;
+  width: 100% !important;
+  height: 100% !important;
+  object-fit: cover;
+  z-index: 0;
 `;
 
 const Overlay = styled.div`
@@ -48,6 +82,7 @@ const Title = styled.h1`
   font-weight: var(--text-bold);
   margin: 0 0 var(--space-2) 0;
   font-family: var(--font-family);
+  z-index: 2;
 `;
 
 const Subtitle = styled.h2`
@@ -55,4 +90,5 @@ const Subtitle = styled.h2`
   font-weight: var(--text-regular);
   margin: 0;
   font-family: var(--font-family);
+  z-index: 2;
 `;

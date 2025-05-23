24865241f7c3f5c4347e32398a5ebd0d5e0a8d7d
Angela Capillo <hello@angelcapillo.com>
2025-05-23 12:38:52 +0200

    Remove console logs

--- components/HeroCard.jsx
+++ components/HeroCard.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Image from "next/image";
 export default function HeroCard({ title, children, bgImage }) {
-  console.log("bgImage",bgImage)
+
   return (
     <Wrapper>
       <StyledImage
